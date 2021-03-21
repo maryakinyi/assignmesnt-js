b7275fdc538ca25ebde9c35d5e16110ac095b6e2
maryakinyi <owuorakinyimary@gmail.com>
2021-03-21 18:58:10 +0300

    javasciptwork3

---
+++ java.js
@@ -0,0 +1,26 @@
+grades()
+
+function grades() {
+    var grades = String;
+    if (grades >= 80) {
+        console.log("A");
+    } else
+    if (grades >= 70) {
+        console.log("B");
+    } else if (grades >= 60) {
+        console.log("C");
+    } else if (grades >= 50) {
+        console.log("D");
+    } else if (grades <= 49) {
+        console.log("E");
+    } else if (grades >= "F") {
+        console.log("F");
+    }
+
+
+
+
+
+
+
+}
